2c219181f45ddb7a380e9aeb68e5b0c0739fd8ab
Horvath-B <bence.horvath5@stud.uni-corvinus.hu>
2024-11-30 18:25:40 +0100

    javítások, shop oldallal haladás

---
+++ scripts/script_shop.js
@@ -0,0 +1,10 @@
+function show_cats(){
+    document.getElementById("s_cats").style.display = "flex";
+    document.getElementById("s_habitats").style.display = "none";
+}
+function show_habitats(){
+    document.getElementById("s_cats").style.display = "none";
+    document.getElementById("s_habitats").style.display = "flex";
+}
+document.getElementById('b_cats').addEventListener('click',show_cats)
+document.getElementById('b_habitats').addEventListener('click',show_habitats)
