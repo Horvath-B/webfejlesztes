a4b664cc3964e09751d687f8e7a21432a276fa4c
Horvath-B <bence.horvath5@stud.uni-corvinus.hu>
2024-12-02 20:47:04 +0100

    Main befejezése
    Képek, animáció

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -1,5 +1,6 @@
 num = 1;
 h_num = 1;
+f = 1;
 ccatt=true;
 hhabb=true;
 function cat_next(){
@@ -46,11 +47,43 @@ function h_c(){
         ccatt = true;
     }
 }
+percent = 80;
+function feed(){
+    document.getElementById("f_pic").src = `../images/feeding/f${f}.svg`;
+    document.getElementById('%').innerText =` ${percent}%`
+    f++;
+    percent+=2;
+    if(percent>100) percent = 100;
+    if(f<16) setTimeout(feed,200)
+    else{
+        f = 1;
+        //document.getElementById("f_pic").src = `../images/feeding/f0.svg`;
+    }
+}
+deg = 0.5;
+a = 0;
+hap = 76;
+function zzz(){
+    deg = deg * -1;
+    document.getElementById('cat_pic').style.transform = `rotate(${deg}deg)`;
+    document.getElementById('dor').style.display = 'block';
+    document.getElementById('%2').innerText = `${hap}%`;
+    a++;
+    hap++;
+    if (hap>100) hap = 100;
+    if(a<10)setTimeout(zzz,100);
+    else{
+        a = 0;
+        document.getElementById('dor').style.display = 'none';
+    }
+}
 
 
 document.getElementById('c_prev').addEventListener('click',cat_prev);
 document.getElementById('c_next').addEventListener('click', cat_next);
 document.getElementById('c_c_s').addEventListener('click', c_c_s);
-document.getElementById('c_prev').addEventListener('click',hab_prev);
-document.getElementById('c_next').addEventListener('click', hab_next);
+document.getElementById('h_prev').addEventListener('click',hab_prev);
+document.getElementById('h_next').addEventListener('click', hab_next);
 document.getElementById('h_c').addEventListener('click', h_c);
+document.getElementById('f_s').addEventListener('click', feed)
+document.getElementById('cat_pic').addEventListener('click', zzz)
